704fec96e2caf5bdfedfb5d25ce36bbe3b59e22c
jimhuertas123 <jhuertas@unsa.edu.pe>
2024-11-04 23:45:06 -0500

    FEAT: drawer states changing, but not screens conecteds

--- lib/presentation/features/custom_drawer_feature.dart
+++ lib/presentation/features/custom_drawer_feature.dart
@@ -1,181 +1,168 @@
+import 'package:cube_timer_2/presentation/widgets/drawer_widgets/custom_listtile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:cube_timer_2/presentation/widgets/widgets.dart';
 import 'package:cube_timer_2/presentation/providers/menu_options.dart';
 import 'package:cube_timer_2/config/config.dart';
 
 class DrawerHome extends ConsumerWidget {
   const DrawerHome({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final menuOptions = ref.watch(menuOptionsNotifierProvider).options;
-    final int actualOption =
+    final int selectedOption =
         ref.watch(menuOptionsNotifierProvider).actualOption;
     return SafeArea(
       top: false,
       child: Drawer(
         backgroundColor: Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(0), bottomRight: Radius.circular(0)),
+        ),
         child: ListView(
           physics: const ClampingScrollPhysics(),
           children: <Widget>[
-            SafeArea(
-              left: false,
-              child: SizedBox(
-                height: 178,
-                child: headerDrawer,
-              ),
+            SizedBox(
+              height: 148,
+              child: headerDrawer,
             ),
-
-            CustomTile(
-                title: "asdasd",
-                initiallyExpanded: true,
-                trailing: const Icon(Icons.arrow_downward_outlined),
-                children: [
-                  _listTileDrawer(12, const Icon(Icons.abc_rounded), "title", false)
-                ]),
-
-            ...menuOptions.map((element) {
+            const SizedBox(height: 5),
+            ...appMenuScreensItems.map((element) {
               return (element.children == null)
-                  ? (actualOption == element.id)
+                  ? (selectedOption == element.id)
                       ? _listTileDrawer(
-                            element.id, element.icon, element.title, true)
+                          element.id, element.icon, element.title, true, ref)
                       : _listTileDrawer(
-                          element.id, element.icon, element.title, false)
-                  : _listTileExpandable(
-                      context, element.icon, element.title, element.children!);
+                          element.id, element.icon, element.title, false, ref)
+                  : _listTileExpandable(context,element.id, element.icon, element.title,
+                      element.children!, selectedOption, ref);
             }).toList(),
-
-            // Container(
-            //   padding: const EdgeInsets.all(0),
-            //   child: _listTileDrawer(
-            //       Icons.timer_outlined,
-            //       // iconPll,
-            //       'Cronómetro',
-            //       true),
-            // ),
-            // Theme(
-            //   data:
-            //       Theme.of(context).copyWith(dividerColor: Colors.transparent),
-            //   child: Container(
-            //     margin: EdgeInsets.zero,
-            //     padding: EdgeInsets.zero,
-
-            //     // color: Colors.red,
-            //     // height: 20.0,
-            //     child: ExpansionTile(
-            //       shape: const Border(),
-            //       title: Text("xd"),
-            //       tilePadding: EdgeInsets.zero,
-            //       children: [
-            //         _listTileDrawer(
-            //           Icons.folder_outlined,
-            //           'Exportar/Importar',
-            //           false,
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // ),
-            // const Divider(color: Colors.grey),
-            // const Padding(
-            //     padding: EdgeInsets.only(left: 15.0, top: 8.0, bottom: 15.0),
-            //     child: Text('Otros',
-            //         style:
-            //             TextStyle(fontFamily: 'Arial', color: Colors.black87))),
-            // _listTileDrawer(
-            //   Icons.folder_outlined,
-            //   'Exportar/Importar',
-            //   false,
-            // ),
-            // _listTileDrawer(
-            //     Icons.palette_outlined, 'Tema de la Aplicación', false),
-            // _listTileDrawer(
-            //     Icons.format_paint_outlined, 'Esquema de colores', false),
-            // const Divider(color: Colors.grey),
-            // _listTileDrawer(Icons.settings_outlined, 'Ajustes', false),
-            // _listTileDrawer(Icons.favorite_outline, 'Donar', false),
-            // _listTileDrawer(
-            //     Icons.help_outline, 'Acerca de y comentarios', false),
-            // const SizedBox(height: 25)
+            const Divider(color: Color.fromRGBO(158, 158, 158, 0.4)),
+            const Padding(
+                padding: EdgeInsets.only(left: 14.0, top: 6.0, bottom: 20.0),
+                child: Text('Otros',
+                    style: TextStyle(
+                        fontFamily: 'Arial',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey))),
+            ...appMenuOthers.map((element) {
+              return (selectedOption == element.id)
+                  ? _listTileDrawer(
+                      element.id, element.icon, element.title, true, ref)
+                  : _listTileDrawer(
+                      element.id, element.icon, element.title, false, ref);
+            }).toList(),
+            const Divider(color: Color.fromRGBO(158, 158, 158, 0.4)),
+            ...appMenufinalItems.map((element) {
+              return (selectedOption == element.id)
+                  ? _listTileDrawer(
+                      element.id, element.icon, element.title, true, ref)
+                  : _listTileDrawer(
+                      element.id, element.icon, element.title, false, ref);
+            }).toList(),
+            const SizedBox(height: 5)
           ],
         ),
       ),
     );
   }
 
-  _listTileExpandable(
-      BuildContext context, icon, String title, List<MenuItem> items) {
-    return ListTileTheme(
-      child: ExpansionTile(
-        tilePadding: const EdgeInsets.only(right: 20.0),
-        title: Text(title,
-            style: const TextStyle(fontFamily: 'Arial', fontSize: 15.0)),
-        leading: (icon is IconData)
-            ? Container(
-                // height: 53,
-                padding: const EdgeInsets.only(left: 16, right: 0),
-                color: Colors.red,
-                child: Icon(icon, color: Colors.black54))
-            : icon,
-        // shape: const Border(),
-        // tilePadding: EdgeInsets.zero,
-        children: const [],
+  _listTileExpandable(BuildContext context,int id, IconData icon, String title,
+      List<MenuItem> items, int selectedOption, WidgetRef ref) {
+    return Theme(
+      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          // color: const Color.fromRGBO(244, 67, 54, 0.5),
+        ),
+        child: ExpansionTile(
+            initiallyExpanded: id == 1 
+              ? selectedOption == 2 || selectedOption == 3 ? true : false
+              : id == 4 ? selectedOption == 5 ? true : false : false,
+            collapsedShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            visualDensity: const VisualDensity(horizontal: -4, vertical: -3.5),
+            tilePadding: const EdgeInsets.only(left: 8),
+            title: Padding(
+              padding: const EdgeInsets.only(left: 18.0),
+              child: Text(title,
+                  style: const TextStyle(
+                    fontFamily: 'Arial',
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w500,
+                  )),
+            ),
+            leading: Container(
+                padding: const EdgeInsets.only(left: 0, right: 0),
+                child: Icon(icon, color: Colors.black54)),
+            children: <Widget>[
+              const SizedBox(height: 3),
+              ...items
+                  .map((item) => CustomListTile(
+                        selected: selectedOption == item.id,
+                        icon: item.icon,
+                        title: item.title,
+                        key: Key(item.id.toString()),
+                        onTap: () {
+                          if (item.id != selectedOption) {
+                            ref
+                                .read(menuOptionsNotifierProvider.notifier)
+                                .changeOption(item.id);
+                          }
+                        },
+                      ))
+                  .toList()
+            ]),
       ),
     );
   }
 
-  _listTileDrawer(int id, icon, String title, bool selected) {
-    return ListTile(
-      key: Key(id.toString()),
-      selected: selected,
-      selectedColor: Colors.blue,
-      // selectedTileColor: Colors.red,
-      title: Text(
-            title,
-            style: const TextStyle(fontFamily: 'Arial', fontSize: 15.0)),
-      leading: (icon is IconData) 
-                ? Icon(icon, color: Colors.black54) 
-                : icon,
-      dense: true,
-      visualDensity: const VisualDensity(horizontal: 4),
-      //visualDensity: const VisualDensity(vertical: -2),
-      onTap: () {},
-    );
-  }
+  _listTileDrawer(int id, icon, String title, bool selected, WidgetRef ref) {
+    final int selectedOption =
+        ref.watch(menuOptionsNotifierProvider).actualOption;
 
-  _listItem(dynamic icon, String name) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          // color: Colors.yellow,
-          padding: (icon is IconData)
-              ? const EdgeInsets.only(left: 5.0)
-              : const EdgeInsets.only(left: 31.0, bottom: 12.0),
-          width: 50,
-          height: (icon is IconData) ? 45 : 42,
-          child: (icon is IconData)
-              ? Icon(icon, size: 26.0, color: Colors.black54)
-              : icon,
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: selected
+              ? const Color.fromRGBO(96, 164, 219, 0.13)
+              : Colors.transparent),
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
         ),
-        Container(
-          padding: (icon is IconData)
-              ? const EdgeInsets.only(left: 21.0)
-              : const EdgeInsets.only(left: 35.0),
-          child: (icon is IconData)
-              ? Text(
-                  name,
-                  style: const TextStyle(
-                      fontFamily: 'Arial',
-                      color: Colors.black87,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 15.0),
-                )
-              : Text(name),
-        )
-      ],
+        contentPadding: const EdgeInsets.only(left: 8.0),
+        key: Key(id.toString()),
+        selected: selected,
+        selectedColor: const Color(0xFF4aa8ef),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 18.0),
+          child: Text(title,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Arial',
+                  fontSize: 15.0)),
+        ),
+        leading: Icon(
+          icon,
+          color: selected ? const Color(0xFF4aa8ef) : const Color(0xff767676),
+        ),
+        dense: true,
+        visualDensity: const VisualDensity(horizontal: -4, vertical: -3),
+        onTap: () {
+          if (id == 0 && selectedOption != 0) {
+            // Navigator.of(context).pushNamed('/');
+            ref.read(menuOptionsNotifierProvider.notifier).changeOption(id);
+          }
+        },
+      ),
     );
   }
 }
