051b78d05c1a0039ecaa0b9d343a50f2aabd5db3
jimhuertas123 <jhuertas@unsa.edu.pe>
2025-02-15 22:04:57 -0700

    FEAT: layout sections in timer home page

--- lib/presentation/features/body/main_body/mainbody_containers/cube_containter.dart
+++ lib/presentation/features/body/main_body/mainbody_containers/cube_containter.dart
@@ -5,12 +5,67 @@ class CubeContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 130),
-      color: const Color.fromRGBO(144, 141, 232, 0.3),
-      child: const Center(
-        child: Text('Cube Container'),
-      ),
+    const double spaceFromTop = 130.0;
+    const double spaceFromBottom = 55.0;
+    return Builder(
+      builder: (context) {
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            double containerHeight = constraints.maxHeight - spaceFromTop - spaceFromBottom;
+            double containerWidth = constraints.maxWidth;
+
+            return Container(
+              margin: const EdgeInsets.only(top: 130, bottom: 55),
+              color: Colors.white,
+              height: containerHeight,
+              width: containerWidth,
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: containerHeight*15/100,
+                    color: Color.fromRGBO(100, 100, 100, 0.3),
+                    child: Text('Container Height: $containerHeight')
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                      child: Text('Container Height: $containerHeight'),
+                    )
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: containerHeight*70/100,
+                    color: Colors.red,
+                    
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text('Ao5: 2.45.2'),
+                            Text('Ao12: 123'),
+                            Text('Ao50: 12.3'),
+                            Text('Ao100: 12.00'),
+                          ]
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('Ao5: 2.45.2'),
+                            Text('Ao12: 123'),
+                            Text('Ao50: 12.3'),
+                            Text('Ao100: 12.00'),
+                          ]
+                        )
+                      ]
+                    )
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
