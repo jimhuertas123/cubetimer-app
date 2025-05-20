99711ae1cd0acf64432bf8306635e7d53f69edab
jimhuertas123 <jhuertas@unsa.edu.pe>
2025-05-20 12:43:53 -0500

    FEAT: cube matrix's can clockwise and reverse

--- lib/presentation/features/alert_dialogs/category_selection.dart
+++ lib/presentation/features/alert_dialogs/category_selection.dart
@@ -18,57 +18,56 @@ class CategorySelection extends ConsumerWidget {
     final bool isIOS = Platform.isIOS;
 
     return Column(
-      children: categories
-          .map((category) => ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: isIOS ? CupertinoColors.extraLightBackgroundGray : Colors.transparent,
-                shadowColor: Colors.transparent,
-                overlayColor: isIOS ? null : Colors.grey,
-                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: isIOS ? BorderRadius.circular(10) : BorderRadius.circular(0),
-                  side: BorderSide(
-                    color: Colors.transparent,
-                    width: 0.1,
-                  ),
-                ),
+      children: categories.map((category) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isIOS ? CupertinoColors.extraLightBackgroundGray : Colors.transparent,
+          shadowColor: Colors.transparent,
+          overlayColor: isIOS ? null : Colors.grey,
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+          shape: RoundedRectangleBorder(
+            borderRadius: isIOS ? BorderRadius.circular(10) : BorderRadius.circular(0),
+            side: BorderSide(
+              color: Colors.transparent,
+              width: 0.1,
+            ),
+          ),
+        ),
+        onPressed: () {
+          ref
+            .read(categoryProvider.notifier)
+            .setActualCategory(category.id!);
+          Navigator.of(context).pop();
+        },
+        child: Row(
+          children: <Widget>[
+            const SizedBox(width: 20),
+            Icon(
+              isIOS 
+                ? CupertinoIcons.tag
+                : Icons.label_outline,
+              size: isIOS ? 20 : 25,
+              color: Colors.black38,
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                overflow: TextOverflow.ellipsis,
+                categories.isNotEmpty
+                  ? category.name.length > 22
+                    ? '${category.name.substring(0, 22)}...'
+                    : category.name
+                  : 'No categories available (This must be a error loading categories)',
+                style: isIOS ? null : const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400),
               ),
-              onPressed: () {
-                ref
-                    .read(categoryProvider.notifier)
-                    .setActualCategory(category.id!);
-                Navigator.of(context).pop();
-              },
-              child: Row(
-                children: <Widget>[
-                  const SizedBox(width: 20),
-                  Icon(
-                    isIOS 
-                      ? CupertinoIcons.tag
-                      : Icons.label_outline,
-                    size: isIOS ? 20 : 25,
-                    color: Colors.black38,
-                  ),
-                  const SizedBox(width: 20),
-                  Expanded(
-                    child: Text(
-                      overflow: TextOverflow.ellipsis,
-                      categories.isNotEmpty
-                          ? category.name.length > 22
-                              ? '${category.name.substring(0, 22)}...'
-                              : category.name
-                          : 'No categories available (This must be a error loading categories)',
-                      style: isIOS ? null : const TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  selectedCategoryId == category.id ? Icon(CupertinoIcons.check_mark, color: CupertinoColors.activeBlue,) : const SizedBox.shrink(),
-                  SizedBox(width: 15)
-                ],
-              )))
-          .toList(),
+            ),
+            selectedCategoryId == category.id ? Icon(CupertinoIcons.check_mark, color: CupertinoColors.activeBlue,) : const SizedBox.shrink(),
+            SizedBox(width: 15)
+          ],
+        ))
+      ).toList(),
     );
   }
 }
